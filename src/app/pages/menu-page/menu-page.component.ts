db8b85f8ed38b8086061fbc1347c6c7fef3780c0
Bradley Oosterveen <bradleyoosterveen@gmail.com>
2023-04-26 22:04:47 +0200

    Adjust some things

--- src/app/pages/menu-page/menu-page.component.ts
+++ src/app/pages/menu-page/menu-page.component.ts
@@ -24,14 +24,14 @@ export class MenuPageComponent {
       constitutionScore: 12
     },
     {
-      id: "abcd",
-      name: "Tara Qim",
-      avatarUrl: "https://www.gmbinder.com/images/RL0wHsa.jpg",
-      armorClass: 14,
-      maxHitPoints: 25,
-      strengthScore: 12,
-      dexterityScore: 16,
-      constitutionScore: 12
+      id: "zyxv",
+      name: "Mardok The Cruel",
+      avatarUrl: "https://b2358178.smushcdn.com/2358178/wp-content/uploads/2022/01/HalfOrc-Illustration.jpg?lossy=1&strip=1&webp=1",
+      armorClass: 10,
+      maxHitPoints: 32,
+      strengthScore: 16,
+      dexterityScore: 8,
+      constitutionScore: 15
     },
   ];
 
