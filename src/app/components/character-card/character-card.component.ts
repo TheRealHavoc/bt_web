4c3c9a8fdecd489ff5bd31bea257c81796f1f616
Bradley Oosterveen <bradleyoosterveen@gmail.com>
2023-04-26 22:00:04 +0200

    Add new character buttons

--- src/app/components/character-card/character-card.component.ts
+++ src/app/components/character-card/character-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Character } from 'src/app/models/Character';
 import { AbilityModifier } from 'src/app/utils/ability-modifier';
 
@@ -8,16 +8,7 @@ import { AbilityModifier } from 'src/app/utils/ability-modifier';
   styleUrls: ['./character-card.component.scss']
 })
 export class CharacterCardComponent {
-  public character: Character = {
-    id: "abcd",
-    name: "Tara Qim",
-    avatarUrl: "https://www.gmbinder.com/images/RL0wHsa.jpg",
-    armorClass: 14,
-    maxHitPoints: 25,
-    strengthScore: 12,
-    dexterityScore: 16,
-    constitutionScore: 12
-  }
+  @Input() character: Character | undefined;
 
   public calculateAbilityModifier(score: number): string {
     let modifier = AbilityModifier.calculate(score);
--- src/app/pages/menu-page/menu-page.component.ts
+++ src/app/pages/menu-page/menu-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Character } from 'src/app/models/Character';
 import { AlertService } from 'src/app/services/alert.service';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -9,6 +10,31 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./menu-page.component.scss']
 })
 export class MenuPageComponent {
+  public readonly _MAXCHARACTERCOUNT = 3;
+
+  public characters: Character[] = [
+    {
+      id: "abcd",
+      name: "Tara Qim",
+      avatarUrl: "https://www.gmbinder.com/images/RL0wHsa.jpg",
+      armorClass: 14,
+      maxHitPoints: 25,
+      strengthScore: 12,
+      dexterityScore: 16,
+      constitutionScore: 12
+    },
+    {
+      id: "abcd",
+      name: "Tara Qim",
+      avatarUrl: "https://www.gmbinder.com/images/RL0wHsa.jpg",
+      armorClass: 14,
+      maxHitPoints: 25,
+      strengthScore: 12,
+      dexterityScore: 16,
+      constitutionScore: 12
+    },
+  ];
+
   constructor(
     private authService: AuthService,
     private alertService: AlertService,
