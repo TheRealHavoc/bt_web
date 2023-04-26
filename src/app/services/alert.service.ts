5aa8608532147df9ee67a2e6cb4804a611288c5b
Bradley Oosterveen <bradleyoosterveen@gmail.com>
2023-04-26 20:30:31 +0200

    Update alert timings and badge padding

--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -9,25 +9,27 @@ export class AlertService {
   private _subject = new Subject<Alert>();
   private _id = 0;
 
+  private readonly _TIMEOUT = 6000;
+
   constructor() { }
 
   getObservable(): Observable<Alert> {
     return this._subject.asObservable();
   }
 
-  info(title: string, message?: string, timeout = 4000) {
+  info(title: string, message?: string, timeout = this._TIMEOUT) {
     this._subject.next(new Alert(this._id++, AlertType.info, title, timeout, message));
   }
 
-  success(title: string, message?: string, timeout = 4000) {
+  success(title: string, message?: string, timeout = this._TIMEOUT) {
     this._subject.next(new Alert(this._id++, AlertType.success, title, timeout, message));
   }
 
-  warning(title: string, message?: string, timeout = 4000) {
+  warning(title: string, message?: string, timeout = this._TIMEOUT) {
     this._subject.next(new Alert(this._id++, AlertType.warning, title, timeout, message));
   }
 
-  error(title: string, message?: string, timeout = 4000) {
+  error(title: string, message?: string, timeout = this._TIMEOUT) {
     this._subject.next(new Alert(this._id++, AlertType.error, title, timeout, message));
   }
 }
