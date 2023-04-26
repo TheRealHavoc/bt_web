ec60e2116517c3fe1992f8cc419bdc1f57449e5e
Bradley Oosterveen <bradleyoosterveen@gmail.com>
2023-04-26 19:31:05 +0200

    Remove username from app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { AuthService } from './services/auth.service';
 export class AppComponent {
   title = 'Baldur\'s Trials';
 
-  constructor (public authService: AuthService) {}
+  constructor () {}
 }
