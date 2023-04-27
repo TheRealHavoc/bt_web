27c406208a1c903c49b30d570d29763dd8b77ac0
Bradley Oosterveen <bradleyoosterveen@gmail.com>
2023-04-27 15:58:04 +0200

    Add basic layout for character page

--- src/app/pages/character-page/character-page.component.ts
+++ src/app/pages/character-page/character-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Character } from 'src/app/models/Character';
 import { AuthService } from 'src/app/services/auth.service';
 import { CharacterService } from 'src/app/services/character.service';
+import { AbilityModifier } from 'src/app/utils/ability-modifier';
 
 @Component({
   selector: 'app-character-page',
@@ -10,7 +12,17 @@ import { CharacterService } from 'src/app/services/character.service';
   styleUrls: ['./character-page.component.scss']
 })
 export class CharacterPageComponent {
+  public getAbilityModifier = AbilityModifier.calculate;
+
   public character: Character | undefined;
+
+  form: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    avatarUrl: new FormControl('', Validators.required),
+    strScore: new FormControl('', Validators.required),
+    dexScore: new FormControl('', Validators.required),
+    conScore: new FormControl('', Validators.required)
+  });
 
   constructor(
     public characterService: CharacterService,
@@ -19,5 +31,24 @@ export class CharacterPageComponent {
     const id = this.route.snapshot.paramMap.get('id');
 
     this.character = this.characterService.characters.find(item => item.id == id);
+
+    this.form.get('name')?.setValue(this.character?.name);
+    this.form.get('avatarUrl')?.setValue(this.character?.avatarUrl);
+    this.form.get('strScore')?.setValue(this.character?.strengthScore);
+    this.form.get('dexScore')?.setValue(this.character?.dexterityScore);
+    this.form.get('conScore')?.setValue(this.character?.constitutionScore);
+  }
+
+  public calculateAbilityModifier(score: number): string {
+    let modifier = AbilityModifier.calculate(score);
+
+    if (modifier < 0)
+      return `${modifier}`;
+
+    return `+${modifier}`;
+  }
+
+  public onSubmit() {
+    if (this.form.invalid) return;
   }
 }
