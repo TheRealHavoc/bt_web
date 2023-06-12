3adc80f2fee0c4327a294d49f3f380dabb7317c8
Bradley Oosterveen <bradleyoosterveen@gmail.com>
2023-06-12 21:46:20 +0200

    Fix character is null error

--- src/app/pages/match-page/match-page.component.ts
+++ src/app/pages/match-page/match-page.component.ts
@@ -84,7 +84,7 @@ export class MatchPageComponent implements OnDestroy {
 
     let playerData = this.getUserPlayerData(this.matchService.activeMatch.playerData);
 
-    if (playerData === null) return "";
+    if (playerData === null || playerData.character === null) return "";
 
     if (playerData.character.id === character.id) return "";
 
