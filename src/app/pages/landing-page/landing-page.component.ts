657a7eaf2b9771c5144716c8944af97396ed8753
Bradley Oosterveen <bradleyoosterveen@gmail.com>
2023-07-03 15:56:08 +0200

    Add new image carousel to landing page

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -8,6 +8,14 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent {
+  public images: string[] = [
+    "https://wallpapercave.com/wp/wp2770248.jpg",
+    "https://wallup.net/wp-content/uploads/2019/09/821112-dungeons-dragons-forgotten-realms-magic-rpg-action-adventure-puzzle-fantasy-warrior-dragon-1.jpg",
+    "https://cdn.wallpapersafari.com/69/30/cOTieW.jpg"
+  ];
+
+  public imageIndex: number = 0;
+
   constructor(
     private authService: AuthService,
     private router: Router
@@ -15,5 +23,21 @@ export class LandingPageComponent {
     this.authService.isAuthenticated().then(() => {
       this.router.navigate(['/game'])
     })
+  }
+
+  public incrementImageIndex() {
+    if (this.imageIndex == this.images.length - 1) {
+      this.imageIndex = 0;
+    } else {
+      this.imageIndex++;
+    }
+  }
+
+  public decrementImageIndex() {
+    if (this.imageIndex == 0) {
+      this.imageIndex = this.images.length - 1;
+    } else {
+      this.imageIndex--;
+    }
   }
 }
